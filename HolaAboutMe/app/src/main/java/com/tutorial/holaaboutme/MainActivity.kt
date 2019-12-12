75c3c600019250a5a1411cec6c9496692c1c1c33
matucker <matucker@adaptivehandyapps.com>
2019-12-12 14:45:59 -0500

    HolaAboutMe lección completa

--- HolaAboutMe/app/src/main/java/com/tutorial/holaaboutme/MainActivity.kt
+++ HolaAboutMe/app/src/main/java/com/tutorial/holaaboutme/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.tutorial.holaaboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_done).setOnClickListener {
+            addNickname(it)
+        }
+
+        findViewById<TextView>(R.id.tv_nickname).setOnClickListener {
+            updateNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.et_nickname)
+        val nicknameTextView = findViewById<TextView>(R.id.tv_nickname)
+        nicknameTextView.text = editText.text
+
+        view.visibility = View.GONE
+        editText.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname (view: View) {
+        val editText = findViewById<EditText>(R.id.et_nickname)
+        val doneButton = findViewById<Button>(R.id.btn_done)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        // Set the focus to the edit text.
+        editText.requestFocus()
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
     }
 }
