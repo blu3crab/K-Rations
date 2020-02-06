7bf6e855767b5e5dff308cc22a38ff3fe1189c14
matucker <matucker@adaptivehandyapps.com>
2020-02-06 18:00:07 -0500

    Hola Styles Themes FAB & Search Nav crash app with BG display location not enabled

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -47,14 +47,14 @@ class DevByteApplication : Application() {
 
     private fun setupRecurringWork() {
         val constraints = Constraints.Builder()
-                .setRequiredNetworkType(NetworkType.UNMETERED)
-                .setRequiresBatteryNotLow(true)
-                .setRequiresCharging(true)
-                .apply {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                        setRequiresDeviceIdle(true)
-                    }
-                }
+//                .setRequiredNetworkType(NetworkType.UNMETERED)
+//                .setRequiresBatteryNotLow(true)
+//                .setRequiresCharging(true)
+//                .apply {
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                        setRequiresDeviceIdle(true)
+//                    }
+//                }
                 .build()
 
 //        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS).build()
